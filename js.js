836a5adceada7abe99e505299e6e85f53efd2b4a
nick <nicholasjpenaranda@gmail.com>
2022-07-24 10:37:35 -0500

    create operate function which takes an operator input and two numbers

--- js.js
+++ js.js
@@ -1,15 +1,31 @@
-function add(num, num2) {
+// math functions
+function addF(num, num2) {
     return num + num2;
 };
 
-function subtract(num, num2) {
+function subtractF(num, num2) {
     return num - num2;
 };
 
-function multiply(num, num2) {
+function multiplyF(num, num2) {
     return num * num2;
 };
 
-function divide(num, num2) {
+function divideF(num, num2) {
     return num / num2;
 };
+
+// decides what functions to use
+function operate(operator, num, num2) {
+    switch (operator) {
+        case "add":
+            return addF(num, num2);
+        case subtract:
+            return subtractF(num, num2);
+        case multiply:
+            return multiplyF(num, num2);
+        case divide:
+            return divideF(num, num2);
+    };
+};
+console.log(operate("add", 1, 2));
